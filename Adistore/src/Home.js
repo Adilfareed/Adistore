f1beef3bcde931bc9f607c51c0df71771eb1cbc7
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2023-10-18 11:56:36 -0700

    styled catagory

--- Adistore/src/Home.js
+++ Adistore/src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Catagory from "./components/catagory/Catagory";
 import Banner from "./components/banner/Banner";
+import Products from "./components/Products/Products";
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
     <div className="home">
       <Banner/>
      <Catagory/>
+     <Products/>
     </div>
   );
 };
---
+++ Adistore/src/components/Products/Products.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Product from './product/Product'
+const Products = () => {
+  return (
+    <div><Product/></div>
+  )
+}
+
+export default Products
