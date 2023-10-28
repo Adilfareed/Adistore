c28bac27e79a5005daf8c72c653981a9ea7a0ddd
Adil fareed <82055942+Adilfareed@users.noreply.github.com>
2023-10-28 11:36:33 -0700

    completed cart

--- Adistore/src/pages/Cart.js
+++ Adistore/src/pages/Cart.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import {MdClose} from "react-icons/md"
-import{BsCartx} from "react-icons/bs"
+import{BsCartX} from "react-icons/bs"
 import "./Cart.scss"
 
-const Cart = () => {
+const Cart = ({setshowcart}) => {
   return (
     <div className='cart-panel'>
       <div className='opec-layer'>
 
       </div>
-      <div className='cart-content'> this is cart</div>
+      <div className='cart-content'> 
+      <div className='cart-header'>
+        <span className='heading'> Shopping Cart</span>
+        
+        <span className='close-button' onClick={()=>setshowcart(false)}>
+       
+          <MdClose/>
+      </span>
+      </div>
+     {/*div className='empty cart'>
+      
+        <span > <BsCartX/></span>
+        <span>No Item In The Cart</span>
+        <button className='Button'> Return To Shop</button>
+         
+       </div>*/}
+       <div className='cart-footer'>
+        <span className='total' > subtotal: </span>
+        <span className='price'>  1288</span>
+        
+       </div>
+       <div className='check-div'>
+       <button className='check-button'>CHECKOUT</button></div>
+
+      </div>
     </div>   
   )
 }
